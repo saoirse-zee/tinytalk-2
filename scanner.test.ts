5da7cf76968d66fa9b366a24d0ae1c7d714b6459
Emma Smith <emma.smith@reaktor.com>
2021-11-04 16:53:54 -0400

    Add more tests for scanner

--- scanner.test.ts
+++ scanner.test.ts
@@ -1,3 +1,4 @@
+import { TokenType } from "./token.ts";
 import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
 import { scanner } from "./scanner.ts";
 
@@ -8,6 +9,30 @@ Deno.test("Can scan a single paren", () => {
   assertEquals(tokens, [
     {
       type: "(",
+      line: 1,
+    },
+  ]);
+});
+
+Deno.test("Can scan a single angle bracket", () => {
+  const source = "<";
+  const tokens = scanner(source);
+
+  assertEquals(tokens, [
+    {
+      type: "<",
+      line: 1,
+    },
+  ]);
+});
+
+Deno.test("Can scan a single equals", () => {
+  const source = "=";
+  const tokens = scanner(source);
+
+  assertEquals(tokens, [
+    {
+      type: "=",
       line: 1,
     },
   ]);
@@ -28,6 +53,18 @@ Deno.test("Advances to the next line on newline characters", () => {
     {
       type: "(",
       line: 2,
+    },
+  ]);
+});
+
+Deno.test("Can scan a multi-character token", () => {
+  const source = "<=";
+  const tokens = scanner(source);
+
+  assertEquals(tokens, [
+    {
+      type: TokenType.LESS_EQUAL,
+      line: 1,
     },
   ]);
 });
