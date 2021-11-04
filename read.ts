403ae3e0533b3b0ea315b177cc4e0f9d685b3cb7
Emma Smith <emma.smith@reaktor.com>
2021-11-04 10:52:49 -0400

    Give user usage note

--- read.ts
+++ read.ts
@@ -1,4 +1,10 @@
 const sourceFile = Deno.args[0]
+
+if (!sourceFile) {
+    console.log("Make sure you pass a filename to the script!");
+    Deno.exit(1);
+}
+
 console.log(`Got a source file: ${sourceFile}`);
 
 const text = await Deno.readTextFile(sourceFile);
