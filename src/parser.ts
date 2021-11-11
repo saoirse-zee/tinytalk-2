b3b89912ae57552a4290299a169a44e6f5baf81c
Emma Smith <emma.smith@reaktor.com>
2021-11-11 10:01:29 -0500

    Add test for parser

---
+++ src/parser.ts
@@ -0,0 +1,12 @@
+import { Token } from "./token.ts";
+
+/**
+ * Takes Tinytalk tokens and returns an Abstract Syntax Tree
+ * @param tokens An array of tokens
+ * @returns AST
+ */
+function parse(tokens: Token[]) {
+  return {};
+}
+
+export { parse };
